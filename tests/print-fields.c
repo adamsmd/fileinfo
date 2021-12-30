220630e066289ab854bd1bcc840c0f96ef44f972
Michael D. Adams <adamsmd@users.noreply.github.com>
2021-12-30 08:50:52 +0800

    Fix warnings about `%td` and `%zu` in printf

--- tests/print-fields.c
+++ tests/print-fields.c
@@ -1,5 +1,6 @@
 #include <fileinfo/dynamic.h>
 
+#include <inttypes.h>
 #include <stdio.h>
 
 /* TODO: print by offset */
@@ -8,7 +9,10 @@ int main(void) {
   size_t i;
   for (i = 0; i < fileinfo_fields_length; i++) {
     fileinfo_field field = fileinfo_fields[i];
-    printf("offset: %4td size: %2zu name: %s\n", field.offset, field.size, field.name);
+    /* Note: We cast to intmax_t and uintmax_t to avoid warnings about %td and %zu */
+    printf(
+      "offset: %4"PRIiMAX" size: %2"PRIuMAX" name: %s\n",
+      (intmax_t)field.offset, (uintmax_t)field.size, field.name);
   }
 
   return 0;
